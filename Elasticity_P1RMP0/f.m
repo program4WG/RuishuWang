7fc30d6dc25f95233bf04093ae352f621ce055d7
program4WG <1138635578@qq.com>
2016-04-08 12:54:43 +0800

    Delete f.m

--- Elasticity_P1RMP0/f.m
+++
@@ -1,8 +0,0 @@
-function z=f
-global   lambda
-% z=@(x,y)[(3/2+lambda)*sin(x)*sin(y);-(1/2+lambda)*cos(x)*cos(y)];
-%z=@(x,y)[-2;-4*(1+lambda)];
-% z=@(x,y)[sin(x)*sin(y);cos(x)*cos(y)];
-% z=@(x,y)[-3-2*lambda,1+2*lambda];
-z=@(x,y)[-3-2*lambda;1+2*lambda];
-end
